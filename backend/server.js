a648b12b1856ad30f99fb44dfdcc2ef1c26bfcc3
ashwintantri <ashwin.tantri@somaiya.edu>
2018-12-24 19:40:36 +0530

    restructured

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import logger from 'morgan';
+import mongoose from 'mongoose';
+
+const app = express();
+const router = express.Router();
+
+const API_PORT = process.env.API_PORT || 3001;
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(logger('dev'));
+
+// now we can set the route path & initialize the API
+router.get('/', (req, res) => {
+  res.json({ message: 'Hello, World!' });
+});
+
+// Use our router configuration when we call /api
+app.use('/api', router);
+
+app.listen(API_PORT, () => console.log(`Listening on port ${API_PORT}`));
