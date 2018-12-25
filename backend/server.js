e882be48989af6fb66a2f00967c89ad28eac855d
ashwintantri <ashwin.tantri@somaiya.edu>
2018-12-25 14:10:20 +0530

    completed

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import logger from 'morgan';
 import mongoose from 'mongoose';
+import Comment from './models/comment';
 
 const app = express();
 const router = express.Router();
@@ -10,13 +11,61 @@ const API_PORT = process.env.API_PORT || 3001;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(logger('dev'));
-
-// now we can set the route path & initialize the API
+mongoose.connect("mongodb://<username>:<password>@ds245661.mlab.com:45661/restful",{useNewUrlParser:true});
+//(creds frome prev commit wont work!)
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 router.get('/', (req, res) => {
   res.json({ message: 'Hello, World!' });
 });
-
-// Use our router configuration when we call /api
+router.get('/comments',(req,res)=>{
+    Comment.find((err,comments)=>{
+        if(err) return res.json({success:false,error:err});
+        return res.json({success:true,data:comments});
+    });
+});
+router.post('/comments',(req,res)=>{
+    const comment = new Comment();
+    const {author, text} = req.body;
+    if(!author||!text)
+    {
+        return res.json({
+            success:false,
+            error:'You must provide author and comment'
+        });
+    }
+    comment.author = author;
+    comment.text = text;
+    comment.save(err=>{
+        if(err) return res.json({success:false,error:err});
+        return res.json({success:true});
+    });
+});
+router.put('/comments/:commentId',(req,res)=>{
+    const {commentId} = req.params;
+    if(!commentId){
+    return res.json({ success: false, error: 'No comment id provided' });
+  }
+    Comment.findById(commentId,(error,comment)=>{
+        if(error) return res.json({ success: false, error });
+        const {author,text} = req.body;
+        if(author) comment.author = author;
+        if(text) comment.text = text;
+        comment.save(error=>{
+            if (error) return res.json({ success: false, error });
+            return res.json({ success: true });
+        });
+    });
+});
+router.delete('/comments/:commentId', (req, res) => {
+  const { commentId } = req.params;
+  if (!commentId) {
+    return res.json({ success: false, error: 'No comment id provided' });
+  }
+  Comment.remove({ _id: commentId }, (error, comment) => {
+    if (error) return res.json({ success: false, error });
+    return res.json({ success: true });
+  });
+});
 app.use('/api', router);
-
 app.listen(API_PORT, () => console.log(`Listening on port ${API_PORT}`));
--- client/src/CommentForm.js
+++ client/src/CommentForm.js
@@ -1,25 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-const CommentForm = (props)=>{
-    return (
-      <div>
-        <form onSubmit={props.submitContent}>
-            <input type="text" name="author" placeholder="Your name" value={props.author} onChange={props.handleChangeText}/> 
-            <input type="text" name="text" placeholder="Your views" value={props.text} onChange={props.handleTextChange}/> 
-            <button type="submit">Submit</button>
-        </form>
-      </div>
-    );
-  }
+
+const CommentForm = props => (
+  <form onSubmit={props.submitComment}>
+    <input
+      type="text"
+      name="author"
+      placeholder="Your name…"
+      value={props.author}
+      onChange={props.handleChangeText}
+    />
+    <input
+      type="text"
+      name="text"
+      placeholder="Say something..."
+      value={props.text}
+      onChange={props.handleChangeText}
+    />
+    <button type="submit">Submit</button>
+  </form>
+);
+
 CommentForm.propTypes = {
-        submitComment: PropTypes.func.isRequired,
-        handleChangeText: PropTypes.func.isRequired,
-        text: PropTypes.string,
-        author: PropTypes.string,
-    };
+  submitComment: PropTypes.func.isRequired,
+  handleChangeText: PropTypes.func.isRequired,
+  text: PropTypes.string,
+  author: PropTypes.string,
+};
+
 CommentForm.defaultProps = {
-        text: '',
-        author: '',
-    };
+  text: '',
+  author: '',
+};
 
 export default CommentForm;
